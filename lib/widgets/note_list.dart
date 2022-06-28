3698297a491ad15f0ddbeaa591edc5f8428b46fc
mayudev <99561108+mayudev@users.noreply.github.com>
2022-06-28 16:39:21 +0200

    fix: update home note list when a notification is triggered

--- lib/widgets/note_list.dart
+++ lib/widgets/note_list.dart
@@ -1,17 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stickynotifs/models/state.dart';
 import 'package:stickynotifs/pages/details.dart';
 
-class NoteList extends StatelessWidget {
+class NoteList extends StatefulWidget {
   const NoteList({Key? key, required this.current}) : super(key: key);
 
   final bool current;
 
   @override
+  State<NoteList> createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  @override
   Widget build(BuildContext context) {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+
     return Consumer<NoteModel>(builder: ((context, model, child) {
-      final list = current ? model.notes : model.pending;
+      final list = widget.current ? model.notes : model.pending;
       return ListView.builder(
           shrinkWrap: true,
           physics: const ScrollPhysics(),
